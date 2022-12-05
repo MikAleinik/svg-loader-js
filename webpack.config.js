20ad7cdf126fa5e2fe2cf4b67305de0b0f968ea8
MikAleinik <6424156@tut.by>
2022-12-05 17:56:16 +0300

    fix: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require("path");
 
-const baseConfig = {
+module.exports = {
     entry: {
         app: path.resolve(__dirname, './src/index.js'),
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: 'index.js',
+        filename: 'main.js',
         libraryTarget: 'commonjs2'
     }
 };
